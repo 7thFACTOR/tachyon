30286b8921e39a72fd58c6c3dfa938e11a089689
nicusor_nedelcu <nicusor_nedelcu@localhost>
2017-07-10 22:25:13 +0300

    added compositor

---
+++ code/engine/graphics/frame_compositor.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "base/defines.h"
+#include "core/defines.h"
+#include "graphics/types.h"
+#include "core/resources/texture_resource.h"
+
+namespace engine
+{
+class E_API FrameCompositor
+{
+public:
+};
+
+}
